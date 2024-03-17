44b9c76105322cfa7926d413d1d40a613f944987
Tiago Paulino <tpaulino12000@gmail.com>
2024-03-17 15:31:37 -0300

    script

---
+++ docker-compose.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Criando diretório
+mkdir compose
+cd compose/
+
+# Docker compose command
+docker compose
+
+# Download docker-compose.yml para o projeto docker
+wget https://raw.githubusercontent.com/Paulino02/Docker-project/master/compose/docker-compose.yml
+
+# Provisionando os containers
+
+docker compose up -d
+
+# Verificar se todos os containers estão rodando
+docker compose ps
+

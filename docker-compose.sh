8265084f64a5fa7cb0db336f4a85a3a802427c56
Tiago Paulino <151391657+Paulino02@users.noreply.github.com>
2024-04-13 19:36:26 -0300

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -3,17 +3,12 @@
 # Criando diretório
 mkdir compose
 cd compose/
-
 # Docker compose command
 docker compose
-
 # Download docker-compose.yml para o projeto docker
 wget https://raw.githubusercontent.com/Paulino02/Docker-project/master/compose/docker-compose.yml
-
 # Provisionando os containers
-
 docker compose up -d
-
 # Verificar se todos os containers estão rodando
 docker compose ps
 
